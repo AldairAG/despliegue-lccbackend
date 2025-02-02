228bf0e288837dec9f8bd5afae3ec2c1fd297ed0
AldairAG <alagga02@gmail.com>
2025-02-01 23:31:06 -0600

    update API routes to use production server URLs

--- frontend/src/utils/apiRoutes.js
+++ frontend/src/utils/apiRoutes.js
@@ -1,6 +1,6 @@
 export const API_BASE={
-    API_USER:"http://localhost:4000/lcc/user",
-    API_PETICION:"http://localhost:4000/lcc/peticion",
-    API_PERMISO:"http://localhost:4000/lcc/permiso",
-    API_HISTORIAL:"http://localhost:4000/lcc/hist"
+    API_USER:"http://vps-4676674-x.dattaweb.com:8080/lcc/user",
+    API_PETICION:"http://vps-4676674-x.dattaweb.com:8080/lcc/peticion",
+    API_PERMISO:"http://vps-4676674-x.dattaweb.com:8080/lcc/permiso",
+    API_HISTORIAL:"http://vps-4676674-x.dattaweb.com:8080/lcc/hist"
 }
