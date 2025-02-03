2441e4af0686ba7bd3d361bf178e54e1a6e1c845
AldairAG <alagga02@gmail.com>
2025-02-02 22:11:06 -0600

    fix: update API_BASE URLs for local development and update main.js reference in index.html

--- frontend/src/utils/apiRoutes.js
+++ frontend/src/utils/apiRoutes.js
@@ -1,13 +1,13 @@
-/* export const API_BASE={
+export const API_BASE={
     API_USER:"http://vps-4676674-x.dattaweb.com:8080/lcc/user",
     API_PETICION:"http://vps-4676674-x.dattaweb.com:8080/lcc/peticion",
     API_PERMISO:"http://vps-4676674-x.dattaweb.com:8080/lcc/permiso",
     API_HISTORIAL:"http://vps-4676674-x.dattaweb.com:8080/lcc/hist"
 }
- */
-export const API_BASE={
+
+/* export const API_BASE={
     API_USER:"http://localhost:8080/lcc/user",
     API_PETICION:"http://localhost:8080/lcc/peticion",
     API_PERMISO:"http://localhost:8080/lcc/permiso",
     API_HISTORIAL:"http://localhost:8080/lcc/hist"
-}
+} */
